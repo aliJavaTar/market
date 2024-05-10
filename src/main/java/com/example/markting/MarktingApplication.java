085c166d574e64bb767f069a0d976e8a721e8ca4
aliJavaTar <ali77erfagh99@gmail.com>
2024-05-10 16:21:17 +0330

    start

---
+++ src/main/java/com/example/markting/MarktingApplication.java
@@ -0,0 +1,13 @@
+package com.example.markting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarktingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarktingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/markting/MarktingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.markting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarktingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
